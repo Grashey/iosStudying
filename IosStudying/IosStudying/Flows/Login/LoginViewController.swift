d1affd1d537747955a7cce5aa2074a2c6316934c
Grashey <grashey@mail.ru>
2021-05-31 12:16:59 +0300

    Add LoginViewController

---
+++ IosStudying/IosStudying/Flows/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  IosStudying
+//
+//  Created by Aleksandr Fetisov on 31.05.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
