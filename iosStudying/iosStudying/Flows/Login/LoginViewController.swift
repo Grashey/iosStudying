33f706d93cc633f3c77fab202fc7cd9c269ff175
Grashey <grashey@mail.ru>
2021-06-03 21:05:32 +0300

    Remove storyboard, add TabBar

--- iosStudying/iosStudying/Flows/Login/LoginViewController.swift
+++ iosStudying/iosStudying/Flows/Login/LoginViewController.swift
@@ -8,15 +8,63 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
     
-    @IBAction func isLoginButtonTapped(_ sender: UIButton) {
-    }
+    var loginTextField: UITextField!
+    var passwordTextField: UITextField!
+    var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = R.string.localizible.loginViewControllerTitle()
+        configureContent()
         
+        loginButton.addTarget(self, action: #selector(isButtonPressed), for: .touchUpInside)
     }
+    
+    private func configureContent() {
+        self.view.backgroundColor = .white
+        
+        let borderWidth: CGFloat = 1
+        let borderColor : UIColor = .lightGray
+        
+        loginTextField = UITextField()
+        loginTextField.layer.borderWidth = borderWidth
+        loginTextField.layer.borderColor = borderColor.cgColor
+        loginTextField.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(loginTextField)
+        
+        passwordTextField = UITextField()
+        passwordTextField.layer.borderWidth = borderWidth
+        passwordTextField.layer.borderColor = borderColor.cgColor
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(passwordTextField)
+        
+        loginButton = UIButton()
+        loginButton.setTitle(R.string.localizible.loginButtonTitle(), for: .normal)
+        loginButton.setTitleColor(.blue, for: .normal)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(loginButton)
+        
+        NSLayoutConstraint.activate([
+            loginTextField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100),
+            loginTextField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -100),
+            loginTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            loginTextField.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -120),
+            
+            passwordTextField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100),
+            passwordTextField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -100),
+            passwordTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            passwordTextField.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -60),
+            
+            loginButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ])
+    }
+    
+    @objc func isButtonPressed() {
+        let vc = BooksViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
 }
